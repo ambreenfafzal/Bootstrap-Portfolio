4b9450db03cd77f7a589cb5d72964c32648e30b2
aafzal <ambreenfafzal@gmail.com>
2019-07-31 00:57:50 -0500

    Updated images, buttons, github link.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,6 @@
-console.log("Hello")
+
+$("#portfolioNav").on("click", function () {
+    console.log("clicking")
+const portfolio = document.getElementById("secondRow");
+portfolio.scrollIntoView({ block: "end", behavior: "smooth" });
+  });
